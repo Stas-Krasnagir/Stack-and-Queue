7092f454fb554efbabf878ee1f1d20dc2ddde6b5
Stas_Krasnagir <stas220693@gmail.com>
2020-11-23 17:40:55 +0300

    23-11-20

--- Stack.py
+++ Stack.py
@@ -1,1 +1,19 @@
-class Stach:
+class stack:
+    def __init__(self):
+        self.list = []
+
+    def push(self, item):
+        self.list.append(item)
+
+    def pop(self):
+        pop_elem = self.list.pop()
+        return pop_elem
+
+    def empty(self):
+        empty_elem = self.len(list)
+        if empty_elem == 0:
+            return True
+        else:
+            return empty_elem
+
+# оба эти класса должны обладать методами push, pop, empty
